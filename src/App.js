cab8ed7f9f3c80dfd0689491b402ac9a0fc6927f
yamunanavada <yamunanavada@gmail.com>
2018-06-10 20:42:36 -0400

    making edits

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h3>This is where the conversation is. </h3>
         <ConversationsList />
       </div>
     );
